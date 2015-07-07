1cd4c02e30dc15e97f273a081ae69a0c7ba40688
Arjan Frans <arjanfrans.com@gmail.com>
2015-07-07 22:28:37 +0200

    Learned something new about unique_ptr

--- src/engine/window.cpp
+++ src/engine/window.cpp
@@ -1,10 +1,11 @@
+#include "SDL2/SDL.h"
 #include "window.h"
 
 namespace game {
 namespace engine {
 
     Window::Window()
-    : window_(nullptr, nullptr), surface_(nullptr, SDL_FreeSurface), width_(640), height_(480)
+    : window_(nullptr, nullptr), surface_(nullptr, nullptr), width_(640), height_(480)
     {
     }
 
@@ -15,8 +16,7 @@ namespace engine {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
             return false;
         }
-
-        // surface_ = SDL_GetWindowSurface(window_);
+        surface_ = std::unique_ptr<SDL_Surface, void(*)(SDL_Surface*)>(SDL_GetWindowSurface(window_.get()), SDL_FreeSurface);
         SDL_Delay(2000);
         return true;
     }
