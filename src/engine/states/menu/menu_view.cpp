5fbcf15539f12561d751ab3ed14a817692b0257d
Arjan Frans <arjanfrans.com@gmail.com>
2015-08-04 19:05:29 +0200

    Changed resize to use virtual ratio

--- src/engine/states/menu/menu_view.cpp
+++ src/engine/states/menu/menu_view.cpp
@@ -10,6 +10,8 @@
 
 #include "../../utils/logger.h"
 
+const int VIRTUAL_WIDTH = 800;
+const int VIRTUAL_HEIGHT = 600;
 namespace pl {
 
 MenuView::MenuView(std::shared_ptr<MenuState> state) : state{state}, font{std::make_shared<sf::Font>()} {
@@ -44,7 +46,24 @@ void MenuView::createButtons() {
 }
 
 void MenuView::resize(float width, float height) {
-    this->fixedView.setSize(width, height);
+    float VIRTUAL_RATIO = VIRTUAL_WIDTH / VIRTUAL_HEIGHT;
+    float ratio = width / height;
+    float scale = 1;
+    sf::Vector2f crop{0, 0};
+
+    if(ratio > VIRTUAL_RATIO) {
+        scale = height / VIRTUAL_HEIGHT;
+        crop.x = ((width - (VIRTUAL_WIDTH * scale)) / 2) / width;
+    } else if (ratio < VIRTUAL_RATIO) {
+        scale = width / VIRTUAL_WIDTH;
+        crop.y = ((height - (VIRTUAL_HEIGHT * scale)) / 2) / height;
+    } else {
+        scale = width / VIRTUAL_WIDTH;
+    }
+    
+    auto w = (VIRTUAL_WIDTH * scale) / width;
+    auto h = (VIRTUAL_HEIGHT * scale) / height;
+    this->fixedView.setViewport(sf::FloatRect(crop.x, crop.y, w, h));
     return;
 }
 
