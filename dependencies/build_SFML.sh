fd6d9e4268efd73a7587fd8c0a8419a9c586d58b
Arjan Frans <arjanfrans.com@gmail.com>
2015-08-14 22:14:10 +0200

    updated sfml build script

--- dependencies/build_SFML.sh
+++ dependencies/build_SFML.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-sudo apt-get install libx11-dev libxcb1-dev libx11-xcb-dev libxcb-randr0-dev libxcb-image0-dev libgl1-mesa-dev libudev-dev libfreetype6-dev libjpeg-dev libopenal-dev libflac-dev libvorbis-dev -y
+sudo apt-get install -y libx11-dev libxcb1-dev libx11-xcb-dev libxcb-randr0-dev libxcb-image0-dev libgl1-mesa-dev libudev-dev libfreetype6-dev libjpeg-dev libopenal-dev libflac-dev libvorbis-dev -y
+sudo apt-get install -y libboost-filesystem-dev libboost-system-dev
 
 cd SFML
 mkdir -p build
 cd build
-cmake ..
+cmake DCMAKE_BUILD_TYPE=Release ..
 sudo make install
 rm -rf build
--- dependencies/build_yaml-cpp.sh
+++ dependencies/build_yaml-cpp.sh
@@ -2,5 +2,5 @@ sudo apt-get install libboost-all-dev
 cd yaml-cpp
 mkdir -p build
 cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make
